0810c5b8c04e7b8f18b103280ed2707e68d3c0e8
AbouSega <abusega@icloud.com>
2024-02-14 00:50:56 +0000

    Robot Friends first search box

---
+++ src/robots.js
@@ -0,0 +1,71 @@
+export const robots = [
+    {
+        id: 1,
+        name: 'Fatoumata Sylla',
+        username: 'fsndiaye',
+        email: 'fsndiaye@icloud.com'
+    },
+
+    {
+        id: 2,
+        name: 'Abibou Ndiaye',
+        username: 'andiaye',
+        email: 'andiaye@icloud.com'
+    },
+
+    {
+        id: 3,
+        name: 'Saliou Ndiaye',
+        username: 'sndiaye',
+        email: 'sndiaye@icloud.com'
+    },
+
+    {
+        id: 4,
+        name: 'Fatima BIAYE',
+        username: 'fbiaye',
+        email: 'fbiaye@icloud.com'
+    },
+
+    {
+        id: 5,
+        name: 'Mayé Ndiaye',
+        username: 'mndiaye',
+        email: 'mndiaye@icloud.com'
+    },
+
+    {
+        id: 6,
+        name: 'Oumy Ndiaye',
+        username: 'ondiaye',
+        email: 'ondiaye@icloud.com'
+    },
+
+    {
+        id: 7,
+        name: 'Fatou Kiné Ndiaye',
+        username: 'fkndiaye',
+        email: 'fkndiaye@icloud.com'
+    },
+
+    {
+        id: 8,
+        name: 'Ibrahima Ndiaye',
+        username: 'indiaye',
+        email: 'indiaye@icloud.com'
+    },
+
+    {
+        id: 9,
+        name: 'Mame Cissé',
+        username: 'mcisse',
+        email: 'mcisse@icloud.com'
+    },
+
+    {
+        id: 10,
+        name: 'Adama Ndiaye',
+        username: 'adndiaye',
+        email: 'adndiaye@icloud.com'
+    },
+]
